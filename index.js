bb997bf9b7c8d98c7c2390f37344fae28a8b07ba
Jamie Benjamin <bennythedev@gmail.com>
2022-06-07 09:51:18 -0500

    commit tree b9763034c20c9286dc47b337dfa67f72bb192693

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`)
+}
+
+saturdayFun();
+
+function mondayWork(mondayActivity = "go to the office") {
+    return(`This Monday, I will ${mondayActivity}.`);
+};
+
+const fn = mondayWork()
+
+fn();
+
+function wrapAdjective(symbol = "*") {
+    return function nameAdjective(adjective = "special") {
+        return(`You are ${symbol}${adjective}${symbol}!`)
+    };
+    
+}
+
+wrapAdjective("%")("a dedicated programmer");
